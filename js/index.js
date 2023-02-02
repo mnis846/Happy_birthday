774348915e122aec55d9f9f9f606fb7a1a6a45ed
mnis846 <64012551+mnis846@users.noreply.github.com>
2023-02-02 22:14:26 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/dZRhpArsBEc?autoplay=1");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/GqLLzSmLqGU?autoplay=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
